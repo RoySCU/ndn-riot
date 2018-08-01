7332a7a17c7d19fa738462214d3102700d9ad41c
Tianyuan  Yu <royu9710@outlook.com>
2018-08-01 03:03:45 -0700

    update

--- nfl-app.h
+++ nfl-app.h
@@ -6,7 +6,7 @@
     this function is used for ndn-riot app send ipc message to NFL, to start bootstrap 
 */
 
-static int nfl_start_bootstrap(uint8_t BKpub[64], uint8_t BKpvt[32]);
+/*static int nfl_start_bootstrap(uint8_t BKpub[64], uint8_t BKpvt[32]);*/
 
 
 
