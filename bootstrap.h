c5835863e5d7aabff2be15c5930a7b98959d9132
Tianyuan  Yu <royu9710@outlook.com>
2018-08-01 09:41:49 -0700

    debug

--- bootstrap.h
+++ bootstrap.h
@@ -4,12 +4,5 @@
 #include "thread.h"
 #include "random.h"
 #include "xtimer.h"
-#include <hashes/sha256.h>
-#include <app.h>
-#include <ndn.h>
-#include <msg-type.h>
-#include <crypto/ciphers.h>
-#include <uECC.h>
-#include <string.h>
 
 static void *ndn_bootstrap(void *ptr);
