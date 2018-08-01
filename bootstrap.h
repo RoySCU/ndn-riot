2445d45125847290c29d6f89e3cf9de84a74cf7c
Tianyuan  Yu <royu9710@outlook.com>
2018-08-01 09:40:25 -0700

    debug

--- bootstrap.h
+++ bootstrap.h
@@ -5,16 +5,11 @@
 #include "random.h"
 #include "xtimer.h"
 #include <hashes/sha256.h>
-#include <encoding/ndn-constants.h>
 #include <app.h>
 #include <ndn.h>
-#include <encoding/name.h>
-#include <encoding/interest.h>
-#include <encoding/data.h>
 #include <msg-type.h>
 #include <crypto/ciphers.h>
 #include <uECC.h>
 #include <string.h>
-#include <nfl-block.h>
 
 static void *ndn_bootstrap(void *ptr);
