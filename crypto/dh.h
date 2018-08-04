ec730db5de56b0b5d84fd0b64498410531f4c86e
Tianyuan  Yu <royu9710@outlook.com>
2018-08-03 20:28:40 -0700

    add the DH crypto lib

---
+++ crypto/dh.h
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+
+#define MAX 500 //运算中的最高可能位数
+#define NUMP 2 //已提供的素数个数
+#define NUMG 50 //随机数g的位数
+#define swap(x,y) x^=y^=x^=y
+
+char p_set[NUMP][110]=
+{
+    {"3383489120380959248493525538580021146558530486980451874557556432721233835930146323754645777760514961"},
+    {"3383489120380959248493525538580021146558530486980451874557556432721233835930146323754645777760514963"}
+};
+
+//if you don't know how to use it, please refer to https://download.csdn.net/download/wzcyy2121/10186566
+//account and password: Birldlee 19001900aA,
+
+int get_n(char* a);
+
+int get_mul(char* a,char* b);
+
+int get_mod(char* a,char* b);
+
+void reverse(char* a);
+
+void get_a(char* a);
+
+void get_g(char* a);
+
+void get_p(char* a, int n);
+
+void display(char* a);
+
+void sent(int a, char* p, char* g, char* ans);
+
+void rec(int b, char *p, char *g, char *ans);
+
+void get_key(int b, char *rec, char*p, char*g, char *ans);
+
+
+
