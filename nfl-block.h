5a294dcc00fcc4a520340a711696e0d0a37f1f03
Tianyuan  Yu <royu9710@outlook.com>
2018-08-10 13:56:57 -0700

    nfl-block

--- nfl-block.h
+++ nfl-block.h
@@ -1,7 +1,14 @@
+#ifndef NFL_BLOCK_H_
+#define NFL_BLOCK_H_
+
 #include <inttypes.h>
 #include <sys/types.h>
 #include "encoding/block.h"
 #include <thread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 //all these stuff are read only
 typedef struct nfl_key_pair {
@@ -14,3 +21,10 @@ typedef struct nfl_bootstrap_tuple {
     ndn_block_t* anchor_cert;
     ndn_block_t* home_prefix;        
 } nfl_bootstrap_tuple_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NFL_BLOCK_H_ */
+/** @} */
