55ec2fcb6a1178e1450d414736e7fb21947ed3e6
Tianyuan  Yu <royu9710@outlook.com>
2018-08-16 17:53:38 -0700

    no route

--- bootstrap.c
+++ bootstrap.c
@@ -549,14 +549,12 @@ void *nfl_bootstrap(void *ptr)
         if (from_nfl.type == NFL_START_BOOTSTRAP) {
             DPRINT("nfl-bootstrap: (pid=%" PRIkernel_pid ") START_BOOTSTRAP message received from pid %"
                 PRIkernel_pid "\n", handle->id, from_nfl.sender_pid);
-                
-            ndn_app_express_bootstrapping_request();  /* where all bootstrapping start */
-            ndn_app_run(handle);
             break;
-        
         }    
     }
 
+    ndn_app_express_bootstrapping_request();  /* where all bootstrapping start */
+    ndn_app_run(handle);
     ndn_app_destroy(handle);
 
     return NULL;
